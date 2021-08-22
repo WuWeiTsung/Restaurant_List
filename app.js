5203b6befaa17526e076c00095f825f733600093
WWT <senby298@gmail.com>
2021-08-22 22:10:23 +0800

    modify express routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const Restaurant = require('./models/restaurant')
+const routes = require('./routes')
 
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -30,93 +31,11 @@ db.once('open', () => {
   console.log('mongoDB connected')
 })
 
-app.use(methodOverride('_method'))
+
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
 
-
-
-// index page
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
-})
-//create restaurant 
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-app.put('/restaurants', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.en_name
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create(
-    { name, name_en, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-// detail 
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('detail', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-//edit 
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const name_en = req.body.en_name
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name,
-        restaurant.name_en = name_en,
-        restaurant.category = category,
-        restaurant.image = image,
-        restaurant.location = location,
-        restaurant.phone = phone,
-        restaurant.google_map = google_map,
-        restaurant.rating = rating,
-        restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console(error))
-})
-
-//delete function
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 
 // //搜尋功能(name or category)
