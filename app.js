41f1c38e2ad53b2eb49ebffe9d0edd1da41b57af
WWT <senby298@gmail.com>
2021-08-22 23:19:18 +0800

    modify index page style(CSS)

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const bodyParser = require('body-parser')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
